85927e63be28ba5e9992cf409c1cde989f9eb8e9
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-18 11:27:19 -0400

    added nodemon. added test route. able to save new users.

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -3,6 +3,7 @@ const { authenticate } = require('../utils/middlewares');
 const { getAllJokes, createUser, login } = require('../controllers');
 
 module.exports = server => {
+  server.get('/', (req, res) => res.send('server running'))
   server.get('/api/jokes', authenticate, getAllJokes);
   server.route('/api/users').post(createUser);
   server.route('/api/login').post(login);
